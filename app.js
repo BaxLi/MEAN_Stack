eba084e9aa5b74308319ce0bcc8f7f9da6f8809f
BaxLi <lilian.baxan@gmail.com>
2020-12-11 12:13:34 +0200

    adj

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
-const express=require('express');
-const app=express();
-const bodyParser=require('body-parser');
-const mongoose=require('mongoose')
-const passport=require('passport')
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+const mongoose = require( 'mongoose' )
+const passport = require( 'passport' )
 
-const keys=require('./config/keys');
+const keys = require( './config/keys' );
 
-const mongoDB=()=>{
+const mongoDB = () =>
+{
     mongoose.connect(
-        keys.mongoURI, 
-        { 
-            useNewUrlParser: true, 
+        keys.mongoURI,
+        {
+            useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
-            useFindAndModify:false
-        })
-    .then(()=>console.log("MONGODB connected"))
-    .catch((err)=>console.log("Error MONGODB +", err))
+            useFindAndModify: false
+        } )
+        .then( () => console.log( "MONGODB connected" ) )
+        .catch( ( err ) => console.log( "Error MONGODB +", err ) )
 }
 
 mongoDB()
 
-app.use(passport.initialize())
-require('./middleware/passport')(passport)
+app.use( passport.initialize() )
+require( './middleware/passport' )( passport )
 
-app.use(require('morgan')('dev'))
+app.use( require( 'morgan' )( 'dev' ) )
 
-app.use('/uploads', express.static('./uploads'))
-app.use(bodyParser.urlencoded({ extended:true}))
-app.use(express.json())
-app.use(require('cors')())
+app.use( '/uploads', express.static( './uploads' ) )
+app.use( express.urlencoded( { extended: true } ) )
+app.use( express.json() )
+app.use( require( 'cors' )() )
 
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/analytics',require('./routes/analytics'))
-app.use('/api/category',require('./routes/category'))
-app.use('/api/order',require('./routes/order'))
-app.use('/api/position',require('./routes/position'))
+app.use( '/api/auth', require( './routes/auth' ) )
+app.use( '/api/analytics', require( './routes/analytics' ) )
+app.use( '/api/category', require( './routes/category' ) )
+app.use( '/api/order', require( './routes/order' ) )
+app.use( '/api/position', require( './routes/position' ) )
+
+if ( process.env.NODE_ENV === 'production' )
+{
+    app.use( express.static( 'client/dist/client' ) )
+    app.get( '*', ( req, res ) =>
+    {
+        res.sendFile(
+            path.resolve(
+                __dirname, 'client', 'dist', 'client', 'index.html'
+            )
+        )
+    } )
+}
 
 module.exports = app
